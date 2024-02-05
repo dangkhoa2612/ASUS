f640ce3380827dd5be62f93f57463f378810675c
dangkhoa2612 <khoandgcs18504@fpt.edu.vn>
2024-02-05 20:44:33 +0700

    first commit

---
+++ OneDrive/Desktop/nextjs-ecommerce/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://Amazona:nLGR5o6TebhW3YZQ@cluster0.y28iael.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET": "'7g}`4WW'`:'DhCyZAXcr$sfaT~x",
+    "REFRESH_TOKEN_SECRET": "T_&>!L~Lv-8NF4;8c^;<z%^3uC6g66C?Rqn:a/VK;p;'#jJ",
+    "PAYPAL_CLIENT_ID": "AQr72pcet9b1BV2tyJXt6ZYgFg1X9ZNyDop11_3j0ItAVod2_thvwuODtgwT9d-LVOPElUOGURHdUh9S",
+    "CLOUD_UPDATE_PRESET": "nextjs_store",
+    "CLOUD_NAME": "amazonaa",
+    "CLOUD_API": "https://api.cloudinary.com/v1_1/amazonaa/samples"
+  }
+}
